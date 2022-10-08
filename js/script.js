96a7f26105020296acc93f3b90267074f59d20dd
Elias Paiva <elias.jpaiva@gmail.com>
2022-10-08 10:19:02 +0200

    password visibility

--- js/script.js
+++ js/script.js
@@ -2,18 +2,31 @@ const username = document.getElementById('username');
 const password = document.getElementById('password');
 const btn = document.getElementById('btn');
 const form = document.getElementById('form');
+const openEye = document.querySelector('.fa-eye');
+const closedEye = document.querySelector('.fa-eye-slash');
 
 const enabledButton = () => {
-    if (username.value.length > 0 && password.value.length > 6) {
+    if (username.value.length > 0 && password.value.length > 5) {
         btn.removeAttribute('disabled');
+        console.log('oi');
+        openEye.style.display = 'block';
     } else {
         btn.setAttribute('disabled', 'disabled');
     }
-}
-  ;
+
+    if (password.value.length < 6) {
+        openEye.style.display = 'none';
+    }
+};
+
+const showPassword = () => {
+    password.type = 'text';
+    openEye.style.display = 'none';
+    closedEye.style.display = 'block';
+};
+
 
 form.addEventListener('input', enabledButton);
-
-
+openEye.addEventListener('click', showPassword);
 
 
