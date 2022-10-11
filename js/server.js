478c8b6e4c465dcc975987833b24ea85a53a21a5
Elias Paiva <elias.jpaiva@gmail.com>
2022-10-11 10:09:19 +0200

    learning about express

---
+++ js/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// app.use((request, response) => {
+//     console.log('we got a new request');
+//     console.dir(request);
+//     response.send('<h1>This is the website</h1>');
+// });
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><button>click here</button>');
+});
+
+app.get('/admin', (req, res) => {
+    res.send('<h1>Admin</h1><button>click here</button>');
+});
+
+app.get('/delete', (req, res) => {
+    res.send('<h1>Delete</h1><button>click here</button>');
+});
+
+app.listen(3000, () => {
+    console.log('listening to port 3000');
+});
