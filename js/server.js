e529f6c29b7d08ac886133135c212b2efdd3408e
Elias Paiva <elias.jpaiva@gmail.com>
2022-10-13 18:07:27 +0200

    learning about express

--- js/server.js
+++ js/server.js
@@ -1,33 +1,39 @@
 const express = require('express');
-const app = express();
+const server = express();
 
-// app.use((request, response) => {
+// server.use((request, response) => {
 //     console.log('we got a new request');
 //     console.dir(request);
 //     response.send('<h1>This is the website</h1>');
 // });
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
     res.send('<h1>Home Page</h1><button>click here</button>');
 });
 
-app.get('/admin', (req, res) => {
+server.get('/admin', (req, res) => {
     res.send('<h1>Admin</h1><button>click here</button>');
 });
 
-app.get('/delete', (req, res) => {
+server.get('/delete', (req, res) => {
     res.send('<h1>Delete</h1><button>click here</button>');
 });
 
-app.get('/r/:general', (req, res) => {
-    const { general } = req.params;
-    res.send(`{<h1>The name of this page is${general}</h1>}`);
+server.get('/r/:players/:id', (req, res) => {
+    const { players, id } = req.params;
+    res.send(`<h1>This is ${players}'s page and the id is ${id} </h1>`);
 });
 
-app.get('*', (req, res)=> {
+server.get('/search', (req, res) => {
+    const { q } = req.query;
+    res.send(`<h1>${q}</h1>`);
+});
+
+server.get('*', (req, res)=> {
     res.send('<h1>Bro, I dont know what you are looking for. Sorry!</h1>');
 });
 
-app.listen(3000, () => {
+
+server.listen(3000, () => {
     console.log('listening to port 3000');
 });
