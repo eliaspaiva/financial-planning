853e77a0f79ad2bc7543d3b1171fa79ff10fc145
Elias Paiva <elias.jpaiva@gmail.com>
2022-10-13 17:15:35 +0200

    learning about express

--- js/server.js
+++ js/server.js
@@ -19,6 +19,15 @@ app.get('/delete', (req, res) => {
     res.send('<h1>Delete</h1><button>click here</button>');
 });
 
+app.get('/r/:general', (req, res) => {
+    const { general } = req.params;
+    res.send(`{<h1>The name of this page is${general}</h1>}`);
+});
+
+app.get('*', (req, res)=> {
+    res.send('<h1>Bro, I dont know what you are looking for. Sorry!</h1>');
+});
+
 app.listen(3000, () => {
     console.log('listening to port 3000');
 });
