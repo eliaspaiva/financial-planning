54445fa1694f8dd9d22539c0c51c40e8806e1612
Elias Paiva <elias.jpaiva@gmail.com>
2022-10-17 06:15:01 +0200

    adding logic to input fields

--- public/js/singup.js
+++ public/js/singup.js
@@ -1,9 +1,16 @@
+const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const cPassword = document.getElementById('cPassword');
 const btn = document.getElementById('btn');
 
-if (username.value.length > 0 && email.value.length > 0 && password.value.length > 0 && cPassword.value.length > 0) {
-    btn.removeAttribute('disabled');
-}
+const enabledButton = () => {
+    if (username.value.length > 4 && email.value.length > 0 && password.value.length > 5 && cPassword.value.length > 5 && password.value == cPassword.value) {
+        btn.removeAttribute('disabled');
+    } else {
+        btn.setAttribute('disabled', 'disabled');
+    }
+};
+
+form.addEventListener('input', enabledButton);
