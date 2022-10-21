01fcf9c6e404557813c8153e6d5c742663b12561
Elias Paiva <elias.jpaiva@gmail.com>
2022-10-21 14:45:08 +0200

    adding logic to main page

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,22 @@
 const btnCategory = document.getElementById('btn-category');
-console.log(btnCategory);
+const expenseBox = document.getElementById('expense-box');
+const closeBtn = document.getElementById('close-btn');
+const closeIcon = document.getElementById('closeicon');
+
+// Functions
+const showCategoryButton = () => {
+    expenseBox.style.display = 'block';
+};
+
+const closeCategoryButton = () => {
+    expenseBox.style.display = 'none';
+};
+
+const closeCategoryIcon = () => {
+    expenseBox.style.display = 'none';
+}; 
+
+// Event Listeners
+btnCategory.addEventListener('click', showCategoryButton);
+closeBtn.closeBtn('click', closeCategoryButton);
+closeIcon.addEventListener('click', closeCategoryIcon);
